a015cb21c834ebf7c34cd2a3576fbbafb52e1d62
Manish Chouhan <41690889+Manish5124@users.noreply.github.com>
2023-06-14 14:06:05 +0530

    Add files via upload
    initial commit

---
+++ Helloworld/src/main/java/com/cts/Helloworld/HelloworldApplication.java
@@ -0,0 +1,23 @@
+package com.cts.Helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class HelloworldApplication {
+
+
+	@GetMapping(value = "/message")
+	public String getmessage()
+	{
+		return "Hello world!!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloworldApplication.class, args);
+	}
+
+}
